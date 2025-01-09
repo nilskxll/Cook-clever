e795d13322bbf8178436fdf5a348ea89fcbcc01f
nilskxll <nilskulle17@gmail.com>
2025-01-09 14:56:41 +0100

    Versuch Datenbank und Code zu verknüpfen (Versuch 1)

---
+++ db_connection.php
@@ -0,0 +1,10 @@
+$dsn = 'mysql:host=database-5016834764.webspace-host.com;dbname=dbs13597122';
+$benutzername = 'dbu813693';
+$passwort = 'v!9JBj4^JAcLFTvO';
+
+try {
+$dbh = new PDO($dns, $benutzername, $passwort);
+echo "Verbindung zur Datenbank hergestellt.";
+} catch (PDOException $e) {
+die("Fehler bei der Verbindung zur Datenbank: " . $e->getMessage());
+}
