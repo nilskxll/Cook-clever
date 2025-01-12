2d2568d63bceebd9d9d05d1181334e851d12d7ce
Nils <nilskulle17@gmail.com>
2025-01-12 13:48:37 +0100

    Datenbank connection hergestellt

--- db_connection.php
+++ db_connection.php
@@ -1,10 +1,12 @@
+<?php
 $dsn = 'mysql:host=database-5016834764.webspace-host.com;dbname=dbs13597122';
 $benutzername = 'dbu813693';
 $passwort = 'v!9JBj4^JAcLFTvO';
 
 try {
-$dbh = new PDO($dns, $benutzername, $passwort);
+$dbh = new PDO($dsn, $benutzername, $passwort);
 echo "Verbindung zur Datenbank hergestellt.";
 } catch (PDOException $e) {
 die("Fehler bei der Verbindung zur Datenbank: " . $e->getMessage());
 }
+?>
