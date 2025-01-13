05288711a601991685a6560e2001e14bcfddf2cb
nilskxll <nilskulle17@gmail.com>
2025-01-13 14:55:44 +0100

    Datenbank abfragen weiter erstellt und weiter ausprobiert

--- db_connection.php
+++ db_connection.php
@@ -6,6 +6,15 @@ $passwort = 'v!9JBj4^JAcLFTvO';
 try {
 $dbh = new PDO($dsn, $benutzername, $passwort);
 echo "Verbindung zur Datenbank hergestellt.";
+$erg = $dbh->query("SELECT Arbeitszeit FROM Rezept");
+$rezepte = $erg->fetchAll(PDO::FETCH_ASSOC);
+
+#    print_r($rezepte);
+#if ($erg->num_rows) {
+#    echo "<p>Daten vorhanden: Anzahl ";
+#    echo $erg->num_rows;
+#}
+
 } catch (PDOException $e) {
 die("Fehler bei der Verbindung zur Datenbank: " . $e->getMessage());
 }
