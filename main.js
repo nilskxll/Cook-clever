7971d509abc85f3080d90a29c10d63a48c659bf7
Nils <nilskulle17@gmail.com>
2025-01-16 21:14:08 +0100

    Möglichkeit, bestimmte Dadten eines einzelnen Rezptes auszugeben (Nährwerte)

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ let Nährwerte
 let Rezepte
 let Einheiten
 let Zutaten
+let aktuell_geöffnetes_Rezept = 1 //wird durch Klicken auf rezept dan definiert
+let aktuelle_Nährwerte
+let aktuelles_Rezept
+let aktuelle_Einheiten
+let aktuelle_Zutaten
+let aktuelle_Kalorien
+let aktuelle_Protein
+let aktuelles_Fett
+let aktuelle_Kohlenhydrate
+let aktueller_zugesetzter_Zucken
+let aktuelle_Ballaststoffe
 function daten_aus_db() {
     fetch('cgi-bin/db_connection.php')
         .then(response => response.json())
@@ -11,6 +22,7 @@ function daten_aus_db() {
             Einheiten = daten.einheiten;
             Zutaten = daten.zutaten;
             ausführen()
+            rezept_öffnen()
         })
         .catch(error => {
             console.error("Fehler beim Abrufen der Daten:", error);
@@ -18,10 +30,36 @@ function daten_aus_db() {
 }
 
 function ausführen() {
+    // kann gelöscht werden, jedoch sieht man erstmal wie das dort in den einzelnen Variablen abgespeichert ist.
     document.getElementById("Nährwerte").innerText = JSON.stringify(Nährwerte)
     document.getElementById("Rezepte").innerText = JSON.stringify(Rezepte)
     document.getElementById("Einheiten").innerText = JSON.stringify(Einheiten)
     document.getElementById("Zutaten").innerText = JSON.stringify(Zutaten)
 }
+function rezept_öffnen(){
+    if (aktuell_geöffnetes_Rezept <= Rezepte.length){ //überprüfen ob Rezept überhaupt vorhanden (eventuell unnötig)
+        aktuelle_Nährwerte= Nährwerte[aktuell_geöffnetes_Rezept - 1]
+        aktuelle_Kalorien = aktuelle_Nährwerte.Kalorien;
+        aktuelle_Protein = aktuelle_Nährwerte.Protein;
+        aktuelles_Fett = aktuelle_Nährwerte.Fett;
+        aktuelle_Kohlenhydrate = aktuelle_Nährwerte.Kohlenhydrate;
+        aktueller_zugesetzter_Zucken = aktuelle_Nährwerte.zugesetzer_Zucker;
+        aktuelle_Ballaststoffe = aktuelle_Nährwerte.Ballaststoffe;
 
+        // Optional: Alles als Liste darstellen (nur zum test)
+        let liste = [
+            aktuelle_Kalorien,
+            aktuelle_Protein,
+            aktuelles_Fett,
+            aktuelle_Kohlenhydrate,
+            aktueller_zugesetzter_Zucken,
+            aktuelle_Ballaststoffe,
+        ];
+        console.log("Rezept " + aktuell_geöffnetes_Rezept +":", liste);
+    }
+    else{
+        console.log("Fehler bei dem Rezept öffnen")
+    }
+
+}
 daten_aus_db()
