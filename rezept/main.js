00dcaf634910f6f37c593ee852698beb6cb1ff54
lukasmueller <lukas.mueller.1807@gmx.de>
2025-02-19 22:27:29 +0100

    UI Rezept gewünschte Anzahl an Zutaten durch JS in HTML hinzufügen

--- rezept/main.js
+++ rezept/main.js
@@ -1,6 +1,7 @@
 let minus_button = document.getElementById("minus-button")
 let plus_button = document.getElementById("plus-button")
 let number_of_portions = document.querySelector(".number-of-portions-frame .value")
+let number_of_ingredients
 
 
 minus_button.addEventListener("click", function() {change_number_of_portions("down")})
@@ -18,7 +19,7 @@ function extract_number_of_portions() {
 function change_number_of_portions (direction) {
     let value = extract_number_of_portions()
 
-    // Wert der Portionenzahl ändern
+    // Wert der Portionenzahl ändern (Beschränkung auf max. 99 hat keinen wirklichen Grund. Lässt man mehr zu, müsste der number of portions frame mit wachsen.)
     if (direction === "up") {
         if (value < 99) {
             value++
@@ -35,4 +36,47 @@ function change_number_of_portions (direction) {
     }
 }
 
-// TODO: ausporbieren, wie man eine gewünschte Anzahl an Zutaten durch JS in HTML hinzufügen kann
+// Zutaten mit Namen und Werten einfügen
+function insert_ingredients() {
+    number_of_ingredients = 5
+    let ingredients_frame = document.querySelector(".ingredients-frame")
+    // mit 0 angefangen, dass man aus der ingredients list direkt mit index = i einfügen kann
+    for (let i = 0; i < number_of_ingredients; i++) {
+        // HTML Elemente erstellen
+        /*<div className="ingredient">
+            <div className="name-frame">
+                <div className="name">Name der Zutat</div>
+            </div>
+            <div className="value-frame">
+                <div className="value">Menge der Zutat mit Einheit</div>
+            </div>
+        </div>*/
+        let ingredient = document.createElement("div")
+        ingredient.classList.add("ingredient")
+
+        let name_frame = document.createElement("div")
+        name_frame.classList.add("name-frame")
+
+        let name = document.createElement("div")
+        name.classList.add("name")
+        name.textContent = "Name der Zutat"
+
+        let value_frame = document.createElement("div")
+        value_frame.classList.add("value-frame")
+
+        let value = document.createElement("div")
+        value.classList.add("value")
+        value.textContent = "Menge"
+
+        // Elemente zusammenfügen und in ingredients_frame im HTML Dokument einfügen
+        name_frame.appendChild(name)
+        value_frame.appendChild(value)
+        ingredient.appendChild(name_frame)
+        ingredient.appendChild(value_frame)
+        ingredients_frame.appendChild(ingredient)
+    }
+}
+
+
+
+insert_ingredients()
