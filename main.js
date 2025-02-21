34f625fdd956f31b509b1d1c3af1aa0cc81451d3
lukasmueller <lukas.mueller.1807@gmx.de>
2025-02-21 22:52:06 +0100

    HTML CSS JS einheitlich auf englisch übersetzt

---
+++ main.js
@@ -0,0 +1,20 @@
+let number_of_recipes = 9
+let number_of_recipe_blocks = Math.round(number_of_recipes / 2)
+
+function insert_recipes_blocks() {
+    // Rezepte-Rows einfügen (2 Rezepte-Karten pro Row)
+    let recipes_block = document.querySelector(".block-recipes")
+    let recipes_row = document.querySelector(".recipes-row")
+    for (let i = 0; i < number_of_recipe_blocks - 1; i++) {
+        let clone = recipes_row.cloneNode(true)
+        recipes_block.appendChild(clone)
+    }
+    // letzte Rezepte-Karte entfernen, wenn ungerade Anzahl Rezepte gewünscht ist
+    if (number_of_recipes % 2 === 1) {
+        let recipe_cards_list = Array.from(document.querySelectorAll(".recipe-card"))
+        let last_recipe_card = recipe_cards_list[recipe_cards_list.length - 1]
+        last_recipe_card.style.display = "none"
+    }
+}
+
+insert_recipes_blocks()
