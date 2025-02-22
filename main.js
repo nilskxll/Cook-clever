593243a35ac0d4129af7b1f5b452dce9c216c186
Nils <nilskulle17@gmail.com>
2025-02-22 21:42:46 +0100

    Zufallsgenerator fertig erstellt

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let number_of_recipes = 10
+let number_of_recipes = 5
 let number_of_recipe_blocks = Math.round(number_of_recipes / 2)
 
 // Rezepte-Rows (nach gewünschter Anzahl) erstellen
@@ -20,11 +20,20 @@ function insert_recipes_blocks() {
 
 insert_recipes_blocks()
 
-function Rezepte_auswählen (){ //Zufällig
+function Rezepte_auswählen (){//Zufällig
     let Rezepte_IDs = []
-    for (let i = 0; i < window.Rezepte.length; i++) {
+    let Zufalls_Rezept_IDs = [] //endgültige Liste, in der am ende alle zufällig ausgewählten rezepte drin sind
+    for (let i = 0; i < window.Rezepte.length; i++) { //hier werden alle Rezept_Ids erstmal in eine vollständige Liste eingefügt
         Rezepte_IDs.push(window.Rezepte[i].Rezept_ID)
     }
-    console.log(Rezepte_IDs)
+    for(let i = 0; i < number_of_recipes; i++){ //hier wird die schleife so oft durchlaufen, wie Karten erstellt wurden
+        let j = Math.floor(Math.random() * Rezepte_IDs.length) //hier wird zufällige Zahl generiert die von 0 bis länge der Rezept_Ids Liste gibt
+        Zufalls_Rezept_IDs.push(Rezepte_IDs[j]) //hier in endgültige Liste eingefügt
+        Rezepte_IDs.splice(j,1) //damit nichts doppelt hier Element entfernt
+    }
+    console.log(Zufalls_Rezept_IDs)
+    return Zufalls_Rezept_IDs
 }
-Rezepte_auswählen()
+
+setTimeout(Rezepte_auswählen, 1200)
+//TODO: Anpassen/ Schönere Methode finden, damit alles flüssiger Läuft (direkt nach dem alles definiert ist (von global))
