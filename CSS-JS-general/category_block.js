22dfcc8cae273b10aa5f6b7d8681467c72957986
lukasmueller <lukas.mueller.1807@gmx.de>
2025-02-24 14:51:14 +0100

    Kategorie-Blocks in einem zusammengefasst und coole transition erstellt (nur auf Hauptseite)

--- CSS-JS-general/category_block.js
+++ CSS-JS-general/category_block.js
@@ -1,5 +1,4 @@
-let category_block_1 = document.getElementById("category-block-1")
-let category_block_2 = document.getElementById("category-block-2")
+let category_cards = document.querySelector(".category-cards")
 let category_block_link_icon_right = document.getElementById("category-block-link-icon-right")
 let category_block_link_icon_left = document.getElementById("category-block-link-icon-left")
 let vegan_button = document.getElementById("vegan-button")
@@ -9,8 +8,8 @@ let protein_rich_button = document.getElementById("protein-rich-button")
 let low_calorie_button = document.getElementById("low-calorie-button")
 let cheat_day_button = document.getElementById("cheat-day-button")
 
-category_block_link_icon_right.addEventListener("click", swap_category_block)
-category_block_link_icon_left.addEventListener("click", swap_category_block)
+category_block_link_icon_right.addEventListener("click", move_category_block)
+category_block_link_icon_left.addEventListener("click", move_category_block)
 vegan_button.addEventListener("click", show_vegan_recipes)
 vegetarian_button.addEventListener("click", show_vegetarian_recipes)
 meat_button.addEventListener("click", show_meat_recipes)
@@ -20,15 +19,31 @@ cheat_day_button.addEventListener("click", show_cheat_day_recipes)
 
 
 // angezeigte Kategorien wechseln, beim Klicken auf den Pfeil links bzw. rechts
-function swap_category_block() {
+function move_category_block() {
     // console.log("Kategorie swap gedrückt")
-    if (category_block_1.style.display === "") {
+    category_cards.classList.toggle("second-page")
+    if (category_cards.classList.contains("second-page")) {
+        category_block_link_icon_right.style.opacity = "0"
+        category_block_link_icon_left.style.visibility = ""
+        category_block_link_icon_left.style.opacity = "1"
+        setTimeout(function() {
+            category_block_link_icon_right.style.visibility = "hidden"
+        }, 400)
+    } else {
+        category_block_link_icon_left.style.opacity = "0"
+        category_block_link_icon_right.style.visibility = ""
+        category_block_link_icon_right.style.opacity = "1"
+        setTimeout(function() {
+            category_block_link_icon_left.style.visibility = "hidden"
+        }, 400)
+    }
+    /*if (category_block_1.style.display === "") {
         category_block_1.style.display = "none"
         category_block_2.style.display = ""
     } else {
         category_block_2.style.display = "none"
         category_block_1.style.display = ""
-    }
+    }*/
 }
 
 // auf Suchergebnisse-Seite wechseln mit den Rezepten der gewünschten Kategorie
